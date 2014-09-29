b42913c198823b1dcfb1f8eca9b63e62836c9e6d
Mahedi Azad <mahedi2014@gmail.com>
2014-09-29 11:50:07 +0600

    Encrypt password what submitted index.ctp form. its make a filter to execute before data save to database

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,8 +1,20 @@
 <?php
 App::uses('AppModel', 'Model');
+App::uses('AuthComponent', 'Controller/Component');
+
 
 class User extends AppModel {
 
+    //make password encription before save to database
+    public function beforeSave($options = array())
+    {
+        if (isset($this->data[$this->alias]['password'])) {
+            $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+        }
+        return true;
+    }
+
+    //find all of data of users table
     public function getInfo()
     {
         return $this->find('all');

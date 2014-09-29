ecc0071b30ea403694269fe679e33982aac23292
Mahedi Azad <mahedi2014@gmail.com>
2014-09-29 11:48:28 +0600

    load auth component and session component

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,6 +31,10 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    //load auth component
+    public $component = array('Auth', 'Session');
+
     public function beforeRender()
     {
         $this->layout = 'user';
